485703e398653b0e75059424d313d7485e12456b
30namajnoon <sina.majnoonhjk@gmail.com>
2023-03-04 01:08:57 +0100

    1

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 import Game from "../components/game.js";
 import Control from "../components/control.js";
+import levels from "./levels.js";
 function Index() {
-    
+    this.levels = levels;
+    this.level = localStorage.getItem("level") || 1;
     this.game = new Game(this);
     this.control = new Control(this);
    
---
+++ public/js/levels.js
@@ -0,0 +1,4 @@
+const levels = [
+    {}
+]
+export default levels;
